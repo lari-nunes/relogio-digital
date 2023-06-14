63bf3350f01d5c7421a8360b28036574b32e1058
Larissa Nunes <larissanunes0904@gmail.com>
2023-06-14 19:41:36 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const horas = document.getElementById("horas");
+const minutos = document.getElementById("minutos");
+const segundos = document.getElementById("segundos");
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let seg = dateToday.getSeconds();
+
+    if(hr < 10) {
+        hr = "0" + hr;
+    }else if(min < 10){
+        min = "0" + min
+    }else{
+        seg
+    }    
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg
+
+})
